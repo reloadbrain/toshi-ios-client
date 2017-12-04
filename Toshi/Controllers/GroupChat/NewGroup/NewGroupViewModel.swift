6f414bb52fc9ef1119c77e1d164e8b7010985f4f
yuliaveres <juliaveresklia@gmail.com>
2017-12-04 10:34:10 +0100

    Remove leftover from db connection and fetch from newGroupViewModel

--- Toshi/Controllers/GroupChat/NewGroup/NewGroupViewModel.swift
+++ Toshi/Controllers/GroupChat/NewGroup/NewGroupViewModel.swift
@@ -28,21 +28,6 @@ final class NewGroupViewModel {
 
     private var models: [TableSectionData] = []
 
-    private lazy var uiDatabaseConnection: YapDatabaseConnection = {
-        let database = Yap.sharedInstance.database
-        let dbConnection = database!.newConnection()
-        dbConnection.beginLongLivedReadTransaction()
-
-        return dbConnection
-    }()
-
-    private lazy var mappings: YapDatabaseViewMappings = {
-        let mappings = YapDatabaseViewMappings(groups: [TokenUser.favoritesCollectionKey], view: filteredDatabaseViewName)
-        mappings.setIsReversed(true, forGroup: TokenUser.favoritesCollectionKey)
-
-        return mappings
-    }()
-
     init(_ groupModel: TSGroupModel) {
         groupInfo = GroupInfo()
         groupInfo.title = groupModel.groupName
@@ -61,26 +46,7 @@ final class NewGroupViewModel {
         publicData.tag = NewGroupItemType.isPublic.rawValue
         let publicSectionData = TableSectionData(cellsData: [publicData], headerTitle: Localized("new_group_settings_header_title"))
 
-        //let addParticipantsData = TableCellData(title: Localized("new_group_add_participants_action_title"))
-        //addParticipantsData.tag = NewGroupItemType.addParticipant.rawValue
-        //let addParticipantsSectionData = TableSectionData(cellsData: [addParticipantsData], headerTitle: Localized("new_group_participants_header_title"))
-
         models = [avatarTitleSectionData, publicSectionData]
-    }
-
-    private func contact(at indexPath: IndexPath) -> TokenUser? {
-        var contact: TokenUser?
-
-        uiDatabaseConnection.read { [weak self] transaction in
-            guard let strongSelf = self else { return }
-            guard let dbExtension: YapDatabaseViewTransaction = transaction.extension(strongSelf.filteredDatabaseViewName) as? YapDatabaseViewTransaction else { return }
-
-            guard let data = dbExtension.object(at: indexPath, with: strongSelf.mappings) as? Data else { return }
-
-            contact = TokenUser.user(with: data, shouldUpdate: false)
-        }
-
-        return contact
     }
 
     @objc private func createChat() {
